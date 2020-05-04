0264ad13cc396c1096cde4016a38d143a2d55cfe
dev-coco <63853884+dev-coco@users.noreply.github.com>
2020-05-04 09:40:10 -0400

    Update SystemOptimizationEnable.sh

--- SystemOptimizationEnable.sh
+++ SystemOptimizationEnable.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #Load Auto System Optimization
+chmod +x /Applications/MacCleaner.app/Contents/Resources/AutoSystemOptimization.sh
 cd /Users/$USER/Library/LaunchAgents/ && curl -s https://raw.githubusercontent.com/dev-coco/MacCleaner/master/com.dev-coco.MacCleaner.plist -o com.dev-coco.MacCleaner.plist && sudo chmod +x /Users/$USER/Library/LaunchAgents/com.dev-coco.MacCleaner.plist && launchctl load -w /Users/$USER/Library/LaunchAgents/com.dev-coco.MacCleaner.plist
 
 #Enable Performance Mode
