c05e1651914799490e60b961319db9dd4e5a019d
dev-coco <63853884+dev-coco@users.noreply.github.com>
2020-06-24 23:39:22 -0400

    Update cleaner.sh

--- cleaner.sh
+++ cleaner.sh
@@ -5,25 +5,15 @@ sudo rm -rf /Users/$USER/Library/Application\ Support/MobileSync/Backup/* > /dev
 sudo rm -rf /Users/$USER/Library/iTunes/iPhone\ Software\ Updates/* > /dev/null 2>&1
 
 #Xcode Caches
-sudo rm -rf /Users/$USER/Library/Developer/Xcode/DerivedData/* > /dev/null 2>&1
-sudo rm -rf /Users/$USER/Library/Developer/Xcode/Archives/* > /dev/null 2>&1
-sudo rm -rf /Users/$USER/Library/Developer/Xcode/iOS\ Device\ Logs/* > /dev/null 2>&1
-sudo rm -rf /Users/$USER/Library/Developer/CoreSimulator/Devices/* > /dev/null 2>&1
+sudo rm -rf /Users/$USER/Library/Developer/Xcode/*/* > /dev/null 2>&1
 
 #System Caches
 sudo rm -rf /private/var/log/* > /dev/null 2>&1
 sudo rm -rf /Users/$USER/Library/Logs/* > /dev/null 2>&1
-sudo rm -rf /Users/$USER/Library/Logs/fsck_hfs.log > /dev/null 2>&1
 sudo rm -rf /Library/Logs/DiagnosticReports/*.* > /dev/null 2>&1
-sudo rm -rf /private/var/log/DiagnosticMessages/* > /dev/null 2>&1
 sudo rm -rf /private/var/tmp/com.apple.messages > /dev/null 2>&1
 sudo rm -rf /Users/$USER/Library/Caches/* > /dev/null 2>&1
-sudo rm -rf /Users/$USER/Library/Caches/CloudKit > /dev/null 2>&1
-sudo rm -rf /private/var/db/diagnostics/Special/* > /dev/null 2>&1
-sudo rm -rf /private/var/db/diagnostics/Persist/* > /dev/null 2>&1
-sudo rm -rf /private/var/db/diagnostics/Signpost/* > /dev/null 2>&1
-sudo rm -rf /private/var/db/diagnostics/HighVolume/* > /dev/null 2>&1
-sudo rm -rf /private/var/db/diagnostics/timesync/* > /dev/null 2>&1
+sudo rm -rf /private/var/db/diagnostics/*/* > /dev/null 2>&1
 sudo rm -rf /Library/Logs/DiagnosticReports/ProxiedDevice-Bridge/*.ips > /dev/null 2>&1
 sudo rm -rf /Users/$USER/Library/Application\ Support/CrashReporter/*  > /dev/null 2>&1
 
@@ -39,7 +29,6 @@ sudo rm -rf /Users/$USER/Library/Safari/CloudHistoryRemoteConfiguration.plist > 
 #Chrome Caches
 sudo rm -rf /Users/$USER/Library/Application\ Support/Google/Chrome/*/GPUCache/* > /dev/null 2>&1
 sudo rm -rf /Users/$USER/Library/Application\ Support/Google/Chrome/*/Storage/ext/*/def/GPUCache/* > /dev/null 2>&1
-sudo rm -rf /Users/$USER/Library/Caches/Google/Chrome/PnaclTranslationCache/* > /dev/null 2>&1
 sudo rm -rf /Users/$USER/Library/Application\ Support/Google/Chrome/*/*-journal > /dev/null 2>&1
 sudo rm -rf /Users/$USER/Library/Application\ Support/Google/Chrome/*/databases/*-journal > /dev/null 2>&1
 sudo rm -rf /Users/$USER/Library/Application\ Support/Google/Chrome/*/Visited\ Links > /dev/null 2>&1
