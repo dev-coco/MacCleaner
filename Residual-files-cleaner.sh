0592f3cfd99e11c68212624c8af4da05bf8dbcb5
dev-coco <63853884+dev-coco@users.noreply.github.com>
2020-07-01 17:32:05 -0400

    Update Residual-files-cleaner.sh

--- Residual-files-cleaner.sh
+++ Residual-files-cleaner.sh
@@ -14,6 +14,6 @@ sudo rm -rf /Library/Logs/Avast
 sudo rm -rf /Library/Logs/AvastHUB
 
 #uninstall ccleaner
-sudo rm -rf /Users/$USER/Library/Cookies/com.avast.AAFM.binarycookies
-sudo rm -rf /Users/$USER/Library/Cookies/com.piriform.ccleaner.binarycookies
+#sudo rm -rf /Users/$USER/Library/Cookies/com.avast.AAFM.binarycookies
+#sudo rm -rf /Users/$USER/Library/Cookies/com.piriform.ccleaner.binarycookies
 
