f23f5adb1fe1f8fa8b6162a9d06572265f48c02e
dev-coco <63853884+dev-coco@users.noreply.github.com>
2021-02-26 01:20:05 -0400

    Add files via upload

--- AutoSystemOptimization.sh
+++ AutoSystemOptimization.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #Release RAM
-sudo purge > /dev/null 2>&1
+#sudo purge > /dev/null 2>&1
+#Because the sudo command requires a startup password.
 
 #Clean DNS Caches
 dscacheutil -flushcache > /dev/null 2>&1
