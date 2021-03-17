9cc14f76f025b7e72e9c21768acecfb132f79c0f
dev-coco <63853884+dev-coco@users.noreply.github.com>
2021-03-16 20:50:22 -0400

    Update cleaner_orig.sh

--- cleaner_orig.sh
+++ cleaner_orig.sh
@@ -77,6 +77,8 @@ sudo sqlite3 ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV* '
 #Clean Terminal History
 sudo /bin/rm -rf /Users/$USER/.bash_sessions/* > /dev/null 2>&1
 sudo /bin/rm -rf /Users/$USER/.bash_history > /dev/null 2>&1
+sudo /bin/rm -rf /Users/$USER/.zsh_sessions/* > /dev/null 2>&1
+sudo /bin/rm -rf /Users/$USER/.zsh_history > /dev/null 2>&1
 
 #Applications Caches
 sudo /bin/rm -rf /Users/$USER/Library/Containers/com.apple.QuickTimePlayerX/Data/Library/Caches/* > /dev/null 2>&1
